c8d52d022f983a81e30853c3977d29cab2e20dd5
Emre Akcadag <akcadagemre@gmail.com>
2020-05-20 11:04:51 +0300

    raspi.js gpio added

--- raspi.js
+++ raspi.js
@@ -1,7 +1,10 @@
-const io = require("socket.io-client")("http://localhost:3001");
+const io = require('socket.io-client')("http://192.168.1.102:3001");
+const Gpio = require('onoff').Gpio;
+const relay = new Gpio(17, 'out'); //use GPIO pin 4 as output
+// const pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
 io.on("lightOn", res => {
     const {socketId, data} = res;
-    console.log(socketId);
-    console.log(data);
+    console.log(socketId + " - " + data)
+    relay.writeSync(data);
 });
